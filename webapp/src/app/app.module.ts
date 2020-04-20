8c19ee306784a940a79ab4b183a13cea5ac8caae
Jieying Chen <chen.jiey@husky.neu.edu>
2020-04-20 19:14:04 -0400

    little changes

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TestBlockComponent} from './test-block/test-block.component';
@@ -35,7 +36,7 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     NbLayoutModule,
     NbEvaIconsModule,
     NbSidebarModule,
-    NbButtonModule
+    NbButtonModule,
   ],
   providers: [NbSidebarService],
   bootstrap: [AppComponent]
